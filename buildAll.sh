de4ef2923c4ae5399a6bcc75fa81744dd6e5c4cf
Builderb0y <14227067+Builderb0y@users.noreply.github.com>
2026-03-26 18:19:04 -0500

    remove old version tasks from buildAll.sh.

--- buildAll.sh
+++ buildAll.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-export JAVA_HOME=/home/builderb0y/java/graalvm-jdk-24+36.1/;
+export JAVA_HOME=/home/builderb0y/java/jdk-25+36/
 
-./gradlew "Switch to 1.20.1" && ./gradlew build && \
-./gradlew "Switch to 1.20.2" && ./gradlew build && \
-./gradlew "Switch to 1.20.4" && ./gradlew build && \
-./gradlew "Switch to 1.20.6" && ./gradlew build && \
-./gradlew "Switch to 1.21.1" && ./gradlew build && \
-./gradlew "Switch to 1.21.3" && ./gradlew build && \
-./gradlew "Switch to 1.21.4" && ./gradlew build && \
-./gradlew "Switch to 1.21.5" && ./gradlew build && \
-./gradlew "Switch to 1.21.8" && ./gradlew build && \
-./gradlew "Switch to 1.21.10" && ./gradlew build && \
-./gradlew "Switch to 1.21.11" && ./gradlew build && \
+./gradlew "Switch to 26.1" && ./gradlew build && \
 ./gradlew --stop;
